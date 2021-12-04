8bef50b6efa0f2ee426043f762618e54e367bbcc
Jtobyy <jtobi8161@gmail.com>
2021-12-04 03:09:43 +0000

    Implement 18-binary_tree_uncle.c

--- binary_trees.h
+++ binary_trees.h
@@ -44,5 +44,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 int isvisited(binary_tree_t *node, binary_tree_t **arr, int len, int ind);
 size_t binary_tree_height(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif /* _BINARY_TREE_H_ */
